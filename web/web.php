de5f059351c81ac0f53bcc27dfb2043307c686dc
Sebastian Häni <haeni.sebastian@gmail.com>
2014-10-08 00:41:37 +0200

    added sign in/up pages

--- web/web.php
+++ web/web.php
@@ -5,20 +5,29 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 require_once __DIR__ . '/../bootstrap.php';
 
+// valid pages
+$app ['validPages'] = [ 
+    'home',
+    'developers',
+    'getstarted',
+    'app',
+    'signup',
+    'signin' 
+];
+
 $app->get('/', function () use($app)
 {
     $subRequest = Request::create('/home', 'GET');
     return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
 });
 
-$app['validPages'] = ['home','developers','getstarted','app','register'];
 $app->get('/{page}', function (Application $app, $page) use($app)
 {
-    $templateName = in_array($page, $app['validPages']) ? $page : 'notfound';
-    $template = $app['twig']->loadTemplate($templateName . '.html');
-    return $template->render(array(
-        'config' => $app['config'],
-        'page' => $templateName
+    $templateName = in_array($page, $app ['validPages']) ? $page : 'notfound';
+    $template = $app ['twig']->loadTemplate($templateName . '.html');
+    return $template->render(array (
+        'config' => $app ['config'],
+        'page' => $templateName 
     ));
 });
 
