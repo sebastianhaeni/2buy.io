05afc4d23e8e1e7ce5700747c57b0af94eb17c89
dave <dave@sidefyn.ch>
2014-11-17 21:36:56 +0100

    Create Table Bill & Alter Table Transaction

--- docs/install.sql
+++ docs/install.sql
@@ -7,7 +7,7 @@
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -23,10 +23,13 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `community`;
 CREATE TABLE IF NOT EXISTS `community` (
-  `idCommunity` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
+  `idCommunity` INT(11)      NOT NULL AUTO_INCREMENT,
+  `name`        VARCHAR(200) NOT NULL,
   PRIMARY KEY (`idCommunity`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =latin1
+  AUTO_INCREMENT =1;
 
 -- --------------------------------------------------------
 
@@ -36,13 +39,17 @@ CREATE TABLE IF NOT EXISTS `community` (
 
 DROP TABLE IF EXISTS `community_has_user`;
 CREATE TABLE IF NOT EXISTS `community_has_user` (
-  `idCommunity` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL,
-  `admin` tinyint(1) NOT NULL DEFAULT '0',
-  `receiveNotifications` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`idCommunity`,`idUser`),
+  `idCommunity`          INT(11)      NOT NULL,
+  `idUser`               INT(11)      NOT NULL,
+  `admin`                TINYINT(1)   NOT NULL DEFAULT '0',
+  `bankAccountNumber`    VARCHAR(255) NULL,
+  `bankAccountName`      VARCHAR(255) NULL,
+  `receiveNotifications` TINYINT(1)   NOT NULL DEFAULT '1',
+  PRIMARY KEY (`idCommunity`, `idUser`),
   KEY `idUser` (`idUser`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =latin1;
 
 -- --------------------------------------------------------
 
@@ -52,12 +59,15 @@ CREATE TABLE IF NOT EXISTS `community_has_user` (
 
 DROP TABLE IF EXISTS `invite`;
 CREATE TABLE IF NOT EXISTS `invite` (
-  `idInvite` int(11) NOT NULL AUTO_INCREMENT,
-  `idCommunity` int(11) NOT NULL,
-  `email` varchar(200) NOT NULL,
+  `idInvite`    INT(11)      NOT NULL AUTO_INCREMENT,
+  `idCommunity` INT(11)      NOT NULL,
+  `email`       VARCHAR(200) NOT NULL,
   PRIMARY KEY (`idInvite`),
   KEY `idCommunity` (`idCommunity`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =latin1
+  AUTO_INCREMENT =1;
 
 -- --------------------------------------------------------
 
@@ -67,15 +77,18 @@ CREATE TABLE IF NOT EXISTS `invite` (
 
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
-  `idProduct` int(11) NOT NULL AUTO_INCREMENT,
-  `idCommunity` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `addedBy` int(11) DEFAULT NULL,
-  `inSuggestions` tinyint(1) NOT NULL DEFAULT '0',
+  `idProduct`     INT(11)      NOT NULL AUTO_INCREMENT,
+  `idCommunity`   INT(11)      NOT NULL,
+  `name`          VARCHAR(200) NOT NULL,
+  `addedBy`       INT(11)               DEFAULT NULL,
+  `inSuggestions` TINYINT(1)   NOT NULL DEFAULT '0',
   PRIMARY KEY (`idProduct`),
   KEY `idCommunity` (`idCommunity`),
   KEY `addedBy` (`addedBy`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =latin1
+  AUTO_INCREMENT =1;
 
 -- --------------------------------------------------------
 
@@ -85,15 +98,18 @@ CREATE TABLE IF NOT EXISTS `product` (
 
 DROP TABLE IF EXISTS `remember_me_token`;
 CREATE TABLE IF NOT EXISTS `remember_me_token` (
-  `idToken` int(11) NOT NULL AUTO_INCREMENT,
-  `idUser` int(11) NOT NULL,
-  `token` varchar(255) DEFAULT NULL,
-  `ip` varchar(45) DEFAULT NULL,
-  `userAgent` varchar(150) DEFAULT NULL,
-  `timestampCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `idToken`          INT(11)   NOT NULL AUTO_INCREMENT,
+  `idUser`           INT(11)   NOT NULL,
+  `token`            VARCHAR(255)       DEFAULT NULL,
+  `ip`               VARCHAR(45)        DEFAULT NULL,
+  `userAgent`        VARCHAR(150)       DEFAULT NULL,
+  `timestampCreated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`idToken`),
   KEY `idUser` (`idUser`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =latin1
+  AUTO_INCREMENT =1;
 
 -- --------------------------------------------------------
 
@@ -103,23 +119,56 @@ CREATE TABLE IF NOT EXISTS `remember_me_token` (
 
 DROP TABLE IF EXISTS `transaction`;
 CREATE TABLE IF NOT EXISTS `transaction` (
-  `idTransaction` int(11) NOT NULL AUTO_INCREMENT,
-  `idProduct` int(11) NOT NULL,
-  `reportedBy` int(11) NOT NULL,
-  `reportedDate` datetime NOT NULL,
-  `editedBy` int(11) DEFAULT NULL,
-  `amount` int(11) NOT NULL,
-  `boughtBy` int(11) DEFAULT NULL,
-  `cancelled` tinyint(1) NOT NULL DEFAULT '0',
-  `cancelledBy` int(11) DEFAULT NULL,
-  `closeDate` datetime DEFAULT NULL,
+  `idTransaction` INT(11)    NOT NULL AUTO_INCREMENT,
+  `idProduct`     INT(11)    NOT NULL,
+  `reportedBy`    INT(11)    NOT NULL,
+  `reportedDate`  DATETIME   NOT NULL,
+  `editedBy`      INT(11)             DEFAULT NULL,
+  `amount`        INT(11)    NOT NULL,
+  `boughtBy`      INT(11)             DEFAULT NULL,
+  `cancelled`     TINYINT(1) NOT NULL DEFAULT '0',
+  `cancelledBy`   INT(11)             DEFAULT NULL,
+  `closeDate`     DATETIME            DEFAULT NULL,
+  `price`         DOUBLE(19, 2)       DEFAULT NULL,
+  `idBill`        INT(11)             DEFAULT NULL,
   PRIMARY KEY (`idTransaction`),
   KEY `idProduct` (`idProduct`),
   KEY `reportedBy` (`reportedBy`),
   KEY `editedBy` (`editedBy`),
   KEY `boughtBy` (`boughtBy`),
-  KEY `cancelledBy` (`cancelledBy`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `cancelledBy` (`cancelledBy`),
+  KEY `idBill` (`idBill`)
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =latin1
+  AUTO_INCREMENT =1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+DROP TABLE IF EXISTS `bill`;
+CREATE TABLE IF NOT EXISTS `bill` (
+  `idBill`      INT(11)       NOT NULL AUTO_INCREMENT,
+  `price`       DOUBLE(19, 2) NOT NULL,
+  `picturePath` VARCHAR(255)  NOT NULL,
+  `idCommunity` INT(11)       NOT NULL,
+  `createdBy`   INT(11)       NOT NULL,
+  `createdDate` DATETIME      NOT NULL,
+  `accepted`    TINYINT(1)             DEFAULT NULL,
+  `closedBy`    INT(11)                DEFAULT NULL,
+  `closedDate`  DATETIME               DEFAULT NULL,
+  PRIMARY KEY (`idBill`),
+  KEY `idCommunity` (`idCommunity`),
+  KEY `createdBy` (`createdBy`),
+  KEY `closedBy` (`closedBy`)
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =latin1
+  AUTO_INCREMENT =1;
 
 -- --------------------------------------------------------
 
@@ -129,14 +178,17 @@ CREATE TABLE IF NOT EXISTS `transaction` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `idUser` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `phone` varchar(16) DEFAULT NULL,
+  `idUser`   INT(11)      NOT NULL AUTO_INCREMENT,
+  `name`     VARCHAR(200) NOT NULL,
+  `email`    VARCHAR(200) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `phone`    VARCHAR(16)           DEFAULT NULL,
   PRIMARY KEY (`idUser`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =latin1
+  AUTO_INCREMENT =2;
 
 --
 -- Constraints for dumped tables
@@ -146,34 +198,71 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Constraints for table `community_has_user`
 --
 ALTER TABLE `community_has_user`
-  ADD CONSTRAINT `community_has_user_ibfk_1` FOREIGN KEY (`idCommunity`) REFERENCES `community` (`idCommunity`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `community_has_user_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `community_has_user_ibfk_1` FOREIGN KEY (`idCommunity`) REFERENCES `community` (`idCommunity`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `community_has_user_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 --
 -- Constraints for table `invite`
 --
 ALTER TABLE `invite`
-  ADD CONSTRAINT `invite_ibfk_1` FOREIGN KEY (`idCommunity`) REFERENCES `community` (`idCommunity`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `invite_ibfk_1` FOREIGN KEY (`idCommunity`) REFERENCES `community` (`idCommunity`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 --
 -- Constraints for table `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`idCommunity`) REFERENCES `community` (`idCommunity`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`idCommunity`) REFERENCES `community` (`idCommunity`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 --
 -- Constraints for table `remember_me_token`
 --
 ALTER TABLE `remember_me_token`
-  ADD CONSTRAINT `remember_me_token_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `remember_me_token_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 --
 -- Constraints for table `transaction`
 --
 ALTER TABLE `transaction`
-  ADD CONSTRAINT `transaction_ibfk_5` FOREIGN KEY (`cancelledBy`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `product` (`idProduct`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`reportedBy`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`editedBy`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_ibfk_4` FOREIGN KEY (`boughtBy`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
-SET FOREIGN_KEY_CHECKS=1;
+ADD CONSTRAINT `transaction_ibfk_5` FOREIGN KEY (`cancelledBy`) REFERENCES `user` (`idUser`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `product` (`idProduct`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`reportedBy`) REFERENCES `user` (`idUser`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`editedBy`) REFERENCES `user` (`idUser`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `transaction_ibfk_4` FOREIGN KEY (`boughtBy`) REFERENCES `user` (`idUser`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `transaction_ibfk_6` FOREIGN KEY (`idBill`) REFERENCES `bill` (`idBill`)
+  ON DELETE SET NULL
+  ON UPDATE CASCADE;
+
+--
+-- Constraints for table `bill`
+--
+ALTER TABLE `bill`
+ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`createdBy`) REFERENCES `user` (`idUser`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `bill_ibfk_2` FOREIGN KEY (`closedBy`) REFERENCES `user` (`idUser`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `bill_ibfk_3` FOREIGN KEY (`idCommunity`) REFERENCES `community` (`idCommunity`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+SET FOREIGN_KEY_CHECKS = 1;
