622c193fee41dd30dbee06678212e0d2d8c8130e
Sebastian Häni <haeni.sebastian@gmail.com>
2014-12-06 15:18:51 +0100

    removed grunt-contrib-concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,7 +73,6 @@ module.exports = function(grunt) {
     // Load plugins
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
