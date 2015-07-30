c3281513e3116b1672a645c3fd9a1b1e0b66c2df
Sebastian Häni <haeni.sebastian@gmail.com>
2015-07-30 18:52:19 +0200

    removed unused components because of getting rid of material-ui

--- app/scripts/components/Master.js
+++ app/scripts/components/Master.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Home from '../routes/Home/components/Home';
-import Footer from './Footer';
 
 export default React.createClass({
 
@@ -10,12 +9,8 @@ export default React.createClass({
 
   render() {
     return (
-      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
-        mdl-layout--fixed-header">
-        <main className="mdl-layout__content">
-          {this.props.children || <Home />}
-        </main>
-        <Footer />
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        {this.props.children || <Home />}
       </div>
     );
   }
