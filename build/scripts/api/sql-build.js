299d8acefb71aa7dc8f27db81ca0e763eb06132a
Sebastian Häni <haeni.sebastian@gmail.com>
2015-08-06 17:48:19 +0200

    aggregated generated resources from propel to one dir

--- build/scripts/api/sql-build.js
+++ build/scripts/api/sql-build.js
@@ -1,5 +1,5 @@
 /**
- * Builds the ddl sql and map file. Adds the test data sql to the map file.
+ * Builds the ddl sql file.
  */
 
 var fs = require('fs');
@@ -7,12 +7,4 @@ var path = require('path');
 var execPropel = require('./exec-propel');
 
 console.log('Generating sql based on schema.xml...');
-execPropel(['sql:build', '--overwrite']);
-
-var append = '../build/test-data.sql=default';
-
-fs.appendFile(path.join(__dirname, '/../../../generated-sql/sqldb.map'), append, function(err){
-  if(err){
-    throw err;
-  }
-});
+execPropel(['sql:build']);
